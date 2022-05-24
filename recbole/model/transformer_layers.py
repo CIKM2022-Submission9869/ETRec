b4e76a3690c5177ec96a48f3113bd99e4aff4971
CIKM2022-Submission9869 <105809364+CIKM2022-Submission9869@users.noreply.github.com>
2022-05-25 01:59:45 +0800

    [+] Synthesizer and Performer

--- recbole/model/transformer_layers.py
+++ recbole/model/transformer_layers.py
@@ -249,3 +249,204 @@ class LinformerAttention(nn.Module):
 
         return hidden_states
 
+
+### Synthesizer ###
+class SynthesizerAttention(nn.Module): 
+
+    def __init__(self, n_heads, seq_len, hidden_size, hidden_dropout_prob, attn_dropout_prob, layer_norm_eps):
+        super(SynthesizerAttention, self).__init__()
+        if hidden_size % n_heads != 0:
+            raise ValueError(
+                "The hidden size (%d) is not a multiple of the number of attention "
+                "heads (%d)" % (hidden_size, n_heads))
+        self.seq_len = seq_len
+
+        self.k = int(0.1 * self.seq_len)
+
+        self.num_attention_heads = n_heads
+        self.attention_head_size = int(hidden_size / n_heads)
+        self.all_head_size = self.num_attention_heads * self.attention_head_size
+
+        self.query = nn.Linear(hidden_size, self.k)
+        self.key = nn.Linear(hidden_size, self.k)
+        self.value = nn.Linear(hidden_size, self.all_head_size)
+
+        self.attn_dropout = nn.Dropout(attn_dropout_prob)
+
+        self.dense = nn.Linear(hidden_size, hidden_size)
+        self.LayerNorm = nn.LayerNorm(hidden_size, eps=layer_norm_eps)
+        self.out_dropout = nn.Dropout(hidden_dropout_prob)
+
+    def transpose_for_scores(self, x):
+        new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
+        x = x.view(*new_x_shape)
+        return x.permute(0, 2, 1, 3)
+
+    def forward(self, input_tensor, attention_mask):
+        
+        mixed_query_layer = self.query(input_tensor)
+        mixed_key_layer = self.key(input_tensor) #[1024, 50, 64]
+        mixed_value_layer = self.value(input_tensor)
+
+        value_layer = self.transpose_for_scores(mixed_value_layer)
+
+        attention_scores = torch.matmul(mixed_query_layer, mixed_key_layer.transpose(-1, -2))
+
+        attention_probs = nn.Softmax(dim=-1)(attention_scores)
+        attention_probs = self.attn_dropout(attention_probs)
+        
+        attention_probs = attention_probs.unsqueeze(1)
+        context_layer = torch.matmul(attention_probs, value_layer) #[1024, 2, 50, 32]
+        context_layer = context_layer.permute(0, 2, 1, 3).contiguous() #[1024, 50, 2, 32]
+        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,) #[1024, 50, 64]
+        context_layer = context_layer.view(*new_context_layer_shape) #[1024, 50, 64]
+
+        hidden_states = self.dense(context_layer)
+        hidden_states = self.out_dropout(hidden_states)
+        hidden_states = self.LayerNorm(hidden_states + input_tensor)
+
+        return hidden_states
+
+
+### Performer ###
+class PerformerAttention(nn.Module):
+
+    def __init__(self, config, n_heads, seq_len, hidden_size, hidden_dropout_prob, attn_dropout_prob, \
+                 layer_norm_eps):
+        super(PerformerAttention, self).__init__()
+        if hidden_size % n_heads != 0:
+            raise ValueError(
+                "The hidden size (%d) is not a multiple of the number of attention "
+                "heads (%d)" % (hidden_size, n_heads))
+        ortho_scaling = config['ortho_scaling']
+        qr_uniform_q = config['qr_uniform_q']
+        self.eps = 1e-6
+        self.seq_len = seq_len
+
+        self.num_attention_heads = n_heads
+        self.attention_head_size = int(hidden_size / n_heads)
+        self.all_head_size = self.num_attention_heads * self.attention_head_size
+
+        self.query = nn.Linear(hidden_size, self.all_head_size)
+        self.key = nn.Linear(hidden_size, self.all_head_size)
+        self.value = nn.Linear(hidden_size, self.all_head_size)
+
+        # performer part
+        self.nb_features = int(config['nb_features_ratio'] * self.seq_len)  # int(self.attention_head_size * math.log(self.attention_head_size)) 
+        self.ortho_scaling = ortho_scaling
+        
+        self.create_projection_q = partial(self.gaussian_orthogonal_random_matrix, nb_rows = self.nb_features, nb_columns = self.attention_head_size, scaling = ortho_scaling, qr_uniform_q = qr_uniform_q)
+        self.create_projection_k = partial(self.gaussian_orthogonal_random_matrix, nb_rows = self.nb_features, nb_columns = self.attention_head_size, scaling = ortho_scaling, qr_uniform_q = qr_uniform_q)
+        self.projection_matrix_q = self.create_projection_q()
+        self.projection_matrix_k = self.create_projection_k()
+
+        self.attn_dropout = nn.Dropout(attn_dropout_prob)
+
+        self.dense = nn.Linear(hidden_size, hidden_size)
+        self.LayerNorm = nn.LayerNorm(hidden_size, eps=layer_norm_eps)
+        self.out_dropout = nn.Dropout(hidden_dropout_prob)
+    
+    def orthogonal_matrix_chunk(self, cols, qr_uniform_q = False, device = None):
+        unstructured_block = torch.randn((cols, cols), device = device)
+        q, r = torch.qr(unstructured_block.cpu(), some = True)
+        q, r = map(lambda t: t.to(device), (q, r))
+
+        # proposed by @Parskatt
+        # to make sure Q is uniform https://arxiv.org/pdf/math-ph/0609050.pdf
+        if qr_uniform_q:
+            d = torch.diag(r, 0)
+            q *= d.sign()
+        return q.t()
+
+    def gaussian_orthogonal_random_matrix(self, nb_rows, nb_columns, scaling = 0, qr_uniform_q = False, device = None):
+        nb_full_blocks = int(nb_rows / nb_columns) # 5
+        block_list = []
+
+        for _ in range(nb_full_blocks):
+            q = self.orthogonal_matrix_chunk(nb_columns, qr_uniform_q = qr_uniform_q, device = device)
+            block_list.append(q)
+
+        remaining_rows = nb_rows - nb_full_blocks * nb_columns
+        if remaining_rows > 0:
+            q = self.orthogonal_matrix_chunk(nb_columns, qr_uniform_q = qr_uniform_q, device = device)
+            block_list.append(q[:remaining_rows])
+
+        final_matrix = torch.cat(block_list)
+
+        if scaling == 0:
+            multiplier = torch.randn((nb_rows, nb_columns), device = device).norm(dim = 1)
+        elif scaling == 1:
+            multiplier = math.sqrt((float(nb_columns))) * torch.ones((nb_rows,), device = device)
+        else:
+            raise ValueError(f'Invalid scaling {scaling}')
+        
+        a = torch.diag(multiplier) @ final_matrix
+        return a #[160, 32]
+
+    def transpose_for_scores(self, x):
+        new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
+        x = x.view(*new_x_shape)
+        return x.permute(0, 2, 1, 3)
+
+    def softmax_kernel(self, data, *, projection_matrix, is_query, normalize_data=True, eps=1e-4, device = None):
+        b, h, *_ = data.shape
+
+        data_normalizer = (data.shape[-1] ** -0.25) if normalize_data else 1.
+
+        ratio = (projection_matrix.shape[0] ** -0.5)
+
+        projection = repeat(projection_matrix, 'j d -> b h j d', b = b, h = h)
+        projection = projection.type_as(data)
+        #print('projection', projection.size())
+
+        data_dash = torch.einsum('...id,...jd->...ij', (data_normalizer * data), projection)
+        #print('data_dash1', data_dash.size())
+        diag_data = data ** 2
+        diag_data = torch.sum(diag_data, dim=-1)
+        diag_data = (diag_data / 2.0) * (data_normalizer ** 2)
+        diag_data = diag_data.unsqueeze(dim=-1)
+        #print('diag_data', diag_data.size())
+
+        if is_query:
+            data_dash = ratio * (
+                torch.exp(data_dash - diag_data -
+                        torch.max(data_dash, dim=-1, keepdim=True).values) + eps)
+        else:
+            data_dash = ratio * (
+                torch.exp(data_dash - diag_data - torch.max(data_dash)) + eps)
+        #print('data_dash2', data_dash.size())
+        return data_dash.type_as(data)
+
+    def linear_attention(self, q, k, v):
+        k_cumsum = k.sum(dim = -2)
+        D_inv = 1. / torch.einsum('...nd,...d->...n', q, k_cumsum.type_as(q))
+        context = torch.einsum('...nd,...ne->...de', k, v)
+        out = torch.einsum('...de,...nd,...n->...ne', context, q, D_inv)
+        return out
+
+    def forward(self, input_tensor, attention_mask):
+        mixed_query_layer = self.query(input_tensor)
+        mixed_key_layer = self.key(input_tensor) #[1024, 50, 64]
+        mixed_value_layer = self.value(input_tensor)
+
+        query_layer = self.transpose_for_scores(mixed_query_layer)#[1024, 2, 50, 32]
+        key_layer = self.transpose_for_scores(mixed_key_layer) 
+        value_layer = self.transpose_for_scores(mixed_value_layer)
+
+        create_kernel_q = partial(self.softmax_kernel, projection_matrix = self.projection_matrix_q, device = query_layer.device)
+        create_kernel_k = partial(self.softmax_kernel, projection_matrix = self.projection_matrix_k, device = query_layer.device)
+        query_layer = create_kernel_q(query_layer, is_query = True)
+        key_layer = create_kernel_k(key_layer, is_query = False)
+        #print('query_layer', query_layer.size())
+        context_layer = self.linear_attention(query_layer, key_layer, value_layer)
+
+        context_layer = context_layer.permute(0, 2, 1, 3).contiguous() #[1024, 50, 2, 32]
+        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,) #[1024, 50, 64]
+        context_layer = context_layer.view(*new_context_layer_shape) #[1024, 50, 64]
+
+        hidden_states = self.dense(context_layer)
+        hidden_states = self.out_dropout(hidden_states)
+        hidden_states = self.LayerNorm(hidden_states + input_tensor)
+
+        return hidden_states
+
